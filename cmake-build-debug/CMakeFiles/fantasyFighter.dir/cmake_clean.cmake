35e4c4d4ae6b5e2405855c66f2e138955d6f15e5
Ty Hunt <jtmhunt22@gmail.com>
2021-03-24 17:51:07 -0700

    inital files

---
+++ cmake-build-debug/CMakeFiles/fantasyFighter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fantasyFighter.dir/main.cpp.o"
+  "fantasyFighter"
+  "fantasyFighter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fantasyFighter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
